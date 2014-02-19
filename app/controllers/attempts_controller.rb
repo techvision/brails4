b581d578e8a5e5a702abaaa575081a1e443ee63c
nitin <nitinzd1985@hotmail.com>
2014-02-19 12:52:40 +0530

    Hve corrected the test case from attempt. Have written a create method in attampt controler.

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,2 +1,12 @@
 class AttemptsController < ApplicationController
+
+  def create
+    question_id = params[:question_id]
+    option_id = params[:option_id]
+    Attempt.create_attempt(question_id, option_id, current_user.id)
+
+
+
+  end
+
 end
