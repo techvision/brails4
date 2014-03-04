c2f6547e165c04fb8fd5a83f45c91dd620bcfff8
Nitin Dhaware <nitin@techvision.net.in>
2014-03-04 18:01:13 +0530

    Corrected the test case and have written the code as well.

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -4,5 +4,9 @@ class AttemptsController < ApplicationController
     question_id = params[:question_id]
     option_id = params[:option_id]
     Attempt.create_attempt(question_id, option_id, current_user.id)
+    question = Question.find_by(id: question_id)
+    if question.find_topic.complete?(current_user.id)
+       current_user.profile.update_profile(question.attempt)
+    end
   end
 end
