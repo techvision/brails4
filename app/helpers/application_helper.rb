8aae71e723b6e985eb83e41f633583f5e8e0072f
anuja <anuja@joshsoftware.com>
2014-04-05 16:12:11 +0530

    Issue#10: Add Accessible Panel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,18 +13,6 @@ module ApplicationHelper
 
   end
 
-  def show_flash_messages(options = {})
-    ret = []
-    flash.each do |key, value|
-      ret << content_tag( :div, {class: "flash alert alert-#{key} global", id: "flash-#{key}"}) do
-        content = haml_tag "a.close", "data-dismiss" => "alert" 
-        haml_concat value
-      end
-    end
-    return_string = ret.join("\n")
-    return return_string.respond_to?(:html_safe) ? return_string.html_safe : return_string
-  end
-
   class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
       CodeRay.scan(code, language).div
